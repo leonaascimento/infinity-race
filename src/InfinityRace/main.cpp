25b5f592aef4c58647a9a0c771406eee1837f1ff
Leonardo Nascimento <leonaascimento@gmail.com>
2018-11-27 18:16:13 -0200

    Initial commit

---
+++ src/InfinityRace/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char* argv[]) {
+  QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
+
+  QApplication a(argc, argv);
+  MainWindow w;
+  w.show();
+
+  return a.exec();
+}
